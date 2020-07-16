5eafa911de17397c2560f5d9580bdbfee79d46d0
JNAH Ahmed <eessaouira@gmail.com>
2020-07-16 10:59:40 +0100

    Update redis-client.ts

--- src/connectors/redis-client.ts
+++ src/connectors/redis-client.ts
@@ -1,9 +1,5 @@
 const Redis = require("ioredis");
 
-const redisClient = new Redis({
-  port: 6379,
-  host: 'localhost'
-});
-
+const redisClient = new Redis("redis://:WSnWHF73DgoyAPR1pN8qKhwc0Zv0EqFM@redis-14693.c91.us-east-1-3.ec2.cloud.redislabs.com:14693/0");
 
 export default redisClient;
