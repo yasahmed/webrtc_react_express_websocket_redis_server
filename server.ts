a9094eff3e3e29d009c1da8e143209f5d26328b9
JNAH Ahmed <eessaouira@gmail.com>
2020-07-16 11:36:18 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ export class Server {
 
   private activeSockets: string[] = [];
 
-  private readonly DEFAULT_PORT = 5000;
+  private readonly DEFAULT_PORT = 80;
 
   constructor() {
     this.initialize();
